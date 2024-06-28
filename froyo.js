896b28c0e20a68edc67a168e757d9b704b01f64f
BryceMajor <161669761+BryceMajor@users.noreply.github.com>
2024-06-27 21:07:18 -0400

    Create froyo.js

---
+++ froyo.js
@@ -0,0 +1,22 @@
+// Intial Prompt
+const userEnteredOrder = prompt(
+    "Enter a list separated by commas of all the froyo you would like"
+);
+
+// splits the enetered string at the comma
+const flavor = userEnteredOrder.split(",");
+
+// defines our base
+const froyoOrder = {}
+
+// the function that will proccess the entered order into the table
+for(let i = 0; i < flavor.length; i++){
+    let enteredFlavor = flavor[i]
+    if (enteredFlavor in froyoOrder){
+        froyoOrder[enteredFlavor] += 1
+    } else {
+        froyoOrder[enteredFlavor] = 1
+    }
+}
+
+console.table(froyoOrder)
